5c0c8658eadbc0830835789c3665377dfffaed3d
Neal BasuMullick <nbasumullick11@gmail.com>
2013-04-10 00:28:37 -0300

    Daily dollar volume

---
+++ upgrade-helpers.js
@@ -0,0 +1,3 @@
+// Maximum daily dollar volume for a long position
+
+console.log("$"+parseInt(document.getElementById("sizelimit").innerHTML)*parseFloat(document.getElementById("lastprice").innerHTML)
